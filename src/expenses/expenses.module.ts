ce4e67c28f2db9381c199e21df99d5ee07f19051
Alumasa45 <aluquinn@gmail.com>
2025-11-11 12:10:50 +0300

    expenses module fix

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
 import { Expense } from './entities/expense.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Expense])],
+  imports: [
+    TypeOrmModule.forFeature([Expense]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'mysecretkey',
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '3600s' },
+    }),
+  ],
   controllers: [ExpensesController],
   providers: [ExpensesService],
 })
